fa04547328d80b8de45410ddb2a4fb6fd6189b04
imminentgit <imminentgit@gmail.com>
2023-12-03 12:44:00 +0100

    sm8350-common: Update oplus-fwk

--- oplus-fwk/src/android/os/OplusSystemProperties.java
+++ oplus-fwk/src/android/os/OplusSystemProperties.java
@@ -1,6 +1,13 @@
 package android.os;
 
 public class OplusSystemProperties {
+    public static String get(String key) {
+        return SystemProperties.get(key);
+    }
+
+    public static String get(String key, String def) {
+        return SystemProperties.get(key, def);
+    }
 
     public static int getInt(String key, int def) {
         return SystemProperties.getInt(key, def);
