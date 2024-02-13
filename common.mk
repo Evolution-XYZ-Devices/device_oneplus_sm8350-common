e4d84d1340caa96fb4fb7ef801fb1359108f2246
imminentgit <imminentgit@gmail.com>
2024-02-13 15:08:29 +0100

    sm8350-common: Inherit OnePlus camera

--- common.mk
+++ common.mk
@@ -483,3 +483,6 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8350-common/sm8350-common-vendor.mk)
+
+# Inherit OnePlus camera
+$(call inherit-product-if-exists, vendor/oplus/camera/oplus-camera.mk)
